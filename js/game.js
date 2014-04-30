f0a557800ec78d35759828f2af319390c913fc06
AJ Richardson <aj_r@shaw.ca>
2014-04-30 16:54:13 -0600

    Restructured code to match the desired architecture and added game loop

--- js/game.js
+++ js/game.js
@@ -1,22 +1,33 @@
-﻿// game.js: Game Manager
-function GameManager() {
+﻿function GameManager() {
   var menuActive = false;
-  var display = new DisplayManager();
-  var input = new InputManager();
   var map = new MapManager(display, input);
 
   // Send input commands to the map (for now, at least)
-  input.setInputEventHandlers(map.inputEventHandlers);
+  var inputEventHandlers = map.inputEventHandlers;
 
   // Load the outside map
   map.loadMap("data/UWGmap.json", 272, 160);
 
+  function processInputs(inputQueue) {
+    for (var i = 0; i < inputQueue.length; i++) {
+      var input = inputQueue[i];
+      if (inputEventHandlers[input])
+        inputEventHandlers[input]();
+    }
+    // Clear the array
+    inputQueue.length = 0;
+  }
+
+  // Update the game state given the amount of elapsed time
+  function update(time) {
+    map.update(time);
+  }
+
   return {
     menuActive: menuActive,
-    display: display,
-    input: input,
-    map: map
+    map: map,
+    processInputs: processInputs,
+    update: update
   };
 }
 
-var game = new GameManager();
