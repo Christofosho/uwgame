4dfbbf17a2e4880e06f12606ccc7aa0616e62add
AJ Richardson <aj_r@shaw.ca>
2014-05-01 13:07:48 -0600

    Added event dispatcher, included application.js on main page.

---
+++ js/event.js
@@ -0,0 +1,34 @@
+﻿function EventDispatcher() {
+  var listeners = {};
+  
+  function addEventListener(event, listener) {
+    if (!listeners[event])
+      listeners[event] = [listener];
+    else
+      listeners[event].push(listener);
+  }
+
+  function removeEventListener(event, listener) {
+    if (!listeners[event])
+      return;
+    
+    var index = listeners[event].indexOf(listener);
+    if (index != -1)
+      listeners[event].splice(index, 1);
+  }
+
+  function fireEvent(event, data) {
+    if (!listeners[event])
+      return;
+
+    for (var i in listeners[event]) {
+      listeners[event][i](data);
+    }
+  }
+
+  return {
+    addEventListener: addEventListener,
+    removeEventListener: removeEventListener,
+    fireEvent: fireEvent
+  }
+}
